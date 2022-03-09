2a17d9f9db453ae1e98b2290f024c6452eaf709b
grzes5003 <grzes.kulinski@gmail.com>
2022-03-09 18:43:31 +0100

    [lab01] export env var

--- run_program.sh
+++ run_program.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
+if [[ $# -lt 2 ]] ; then
     echo 'Supply a filename'
     exit 1
 fi
+
+export VARIANT="$2"
 
 mpicc -o prog.out "$1"
 mpiexec -machinefile ./allnodes -np 2 ./prog.out
