703518335d383dd52783190e0218ab62d9490265
grzes5003 <grzes.kulinski@gmail.com>
2022-03-09 22:12:27 +0100

    [lab01] script

--- run_program.sh
+++ run_program.sh
@@ -5,7 +5,14 @@ if [[ $# -lt 2 ]] ; then
     exit 1
 fi
 
+H_PATH=/home/students/k/u/kulinski/repos/MPR-Labs/util
+export LD_LIBRARY_PATH=$H_PATH:$LD_LIBRARY_PATH
+
 export VARIANT="$2"
 
-mpicc -o prog.out "$1"
+gcc -c -Wall -Werror -fpic util/util.o util/helpers.c
+gcc -shared -o util/libutil.so util/util.o
+
+mpicc -L$H_PATH -Wall -o prog.out "$1" -lutil
+
 mpiexec -machinefile ./allnodes -np 2 ./prog.out
