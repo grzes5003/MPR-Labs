b1ce81dbe3ad23b08a0313ea63cfafda9085afc4
grzes5003 <grzes.kulinski@gmail.com>
2022-03-10 23:24:35 +0100

    [lab01] deb

--- lab01/diff_variants.c
+++ lab01/diff_variants.c
@@ -8,8 +8,53 @@
 #include <errno.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../util/helpers.h"
 
+
+int read_env(struct t_env_vars *str) {
+    char path[BUFSIZE];
+    memset(path, 0, sizeof path);
+
+    char *env_variant = "VARIANT";
+    char *msg_size = "MSG_SIZE";
+    char *n_times = "N_TIMES";
+
+    if(!getenv(env_variant)){
+        str->variant = 1;
+    } else if (snprintf(path, BUFSIZE, "%s", getenv(env_variant)) >= BUFSIZE) {
+        fprintf(stderr, "BUFSIZE of %d was too small. Aborting\n", BUFSIZE);
+        return 1;
+    } else if (0L == (str->variant = strtol(path, NULL, 10))) {
+        fprintf(stderr, "Error when parsing\n");
+        return 1;
+    }
+
+    memset(path, 0, sizeof path);
+    if(!getenv(msg_size)){
+        str->msg_size = DEFAULT_MSG_SIZE;
+    } else if (snprintf(path, BUFSIZE, "%s", getenv(msg_size)) >= BUFSIZE) {
+        fprintf(stderr, "MSG_SIZE of %d was too small. Aborting\n", BUFSIZE);
+        return 1;
+    } else if (0L == (str->msg_size = strtol(path, NULL, 10))) {
+        fprintf(stderr, "Error when parsing\n");
+        return 1;
+    }
+
+    memset(path, 0, sizeof path);
+    if(!getenv(n_times)){
+        str->n = DEFAULT_N_TIMES;
+    } else if (snprintf(path, BUFSIZE, "%s", getenv(n_times)) >= BUFSIZE) {
+        fprintf(stderr, "n_times of %d was too small. Aborting\n", BUFSIZE);
+        return 1;
+    } else if (0L == (str->n = strtol(path, NULL, 10))) {
+        fprintf(stderr, "Error when parsing\n");
+        return 1;
+    }
+
+    printf("=== %ld %ld %ld", str->variant, str->msg_size, str->n);
+    return 0;
+}
 
 
 double elapse_time(void (*f)(int, long), int world_rank, struct t_env_vars env_vars) {
