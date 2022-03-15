3e8e51afc255d49ef6902cfea006e43f22901784
grzes5003 <grzes.kulinski@gmail.com>
2022-03-15 13:47:45 +0100

    [LAB02] montecarlo_square.c

---
+++ lab02/montecarlo_square.c
@@ -0,0 +1,37 @@
+//
+// Created by xgg on 15/03/2022.
+//
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+float randomFloat() {
+    return (float)rand()/(float)(RAND_MAX/1);
+}
+
+
+float estimate_pi(int n) {
+    int i;
+    float in = 0.f, out = 0.f;
+    for (i = 0; i < n; i++) {
+        float x = randomFloat();
+        float y = randomFloat();
+        if (sqrt(x * x + y * y) <= 1) {
+            in += 1;
+        } else {
+            out += 1;
+        }
+    }
+
+    return 4.f * out / in;
+}
+
+
+int main(int argc, char *argv[]) {
+    srand((unsigned int)time(NULL));
+    printf("%.6f", estimate_pi(1000));
+    return 0;
+}
+
