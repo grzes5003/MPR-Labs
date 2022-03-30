ef48c36f7d7ba0435811de643590db03bbeb6c8b
grzes5003 <grzes.kulinski@gmail.com>
2022-03-30 12:20:38 +0200

    [lab03] fixes

--- run_lab03.sh
+++ run_lab03.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
+TODAY=$(date +"%d_%H_%M")
 if [ -z "$SCRIPT" ]; then
-  TODAY=$(date +"%d_%H_%M")
   exec 3>&1 4>&2
   trap 'exec 2>&4 1>&3' 0 1 2 3
   exec 1>log_"$TODAY".log 2>&1
@@ -20,3 +20,5 @@ CORES=1
 for ((variant = 1; variant < 4; variant++)); do
   mpiexec -machinefile ./allnodes -n "$CORES" ./"$1" "-n" "$variant"
 done
+
+cat log_"$TODAY".log
