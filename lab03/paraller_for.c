0faeb844ada8d0ba1c0c418a18d25666698553aa
grzes5003 <grzes.kulinski@gmail.com>
2022-03-30 11:53:50 +0200

    [lab03] fixes

--- lab03/paraller_for.c
+++ lab03/paraller_for.c
@@ -4,24 +4,20 @@
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
-#include <time.h>
 #include <getopt.h>
-#include <limits.h>
-#include <errno.h>
-#include <float.h>
 
 
 int main(int argc, char *argv[]) {
 
-    double start, delta;
+    double start; double delta;
     int opt;
     char *end;
 
-    long threads = 1;
+    int threads = 1;
     while (-1 != (opt = getopt(argc, argv, "n:"))) {
         switch (opt) {
             case 'n':
-                threads = strtol(optarg, &end, 10);
+                threads = (int) strtol(optarg, &end, 10);
                 break;
             default:
                 fprintf(stderr, "unexpected argument: %d\n", optopt);
@@ -31,46 +27,26 @@ int main(int argc, char *argv[]) {
     omp_set_num_threads(threads);
 
     const int arr_size = 100000;
-    int nthreads, tid;
+    int nthreads; int tid;
     int *i_tab = malloc(sizeof(int) * arr_size);
 
     start = omp_get_wtime();
 
-#pragma omp parallel private(tid)
-    {
-        /* Obtain thread number */
+#pragma omp parallel default(none) private(tid) shared(nthreads, i_tab, arr_size) //  num_threads(threads)
+{
         tid = omp_get_thread_num();
-//        printf("Hello World from thread = %d\n", tid);
-
-        /* Only master thread does this */
         if (tid == 0) {
             nthreads = omp_get_num_threads();
-//            printf("Number of threads = %d\n", nthreads);
         }
 #pragma omp for
         for (int i = 0; i < arr_size; ++i) {
             i_tab[i] = i;
         }
-    }
+}
 
     delta = omp_get_wtime() - start;
 
     printf("n=%d:delta=%f\n", nthreads, delta);
 
-///* Fork a team of threads giving them their own copies of variables */
-//#pragma omp parallel private(nthreads, tid)
-//    {
-//
-//        /* Obtain thread number */
-//        tid = omp_get_thread_num();
-//        printf("Hello World from thread = %d\n", tid);
-//
-//        /* Only master thread does this */
-//        if (tid == 0) {
-//            nthreads = omp_get_num_threads();
-//            printf("Number of threads = %d\n", nthreads);
-//        }
-//
-//    } /* All threads join master thread and disband */
-
+    return 0;
 }
