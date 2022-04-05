228878f81b135cef21bed5a30fc97e77123029b1
grzes5003 <grzes.kulinski@gmail.com>
2022-04-05 14:28:58 +0200

    [LAB04] sort start

--- lab03/sort_alg_seq.c
+++ lab03/sort_alg_seq.c
@@ -1,6 +1,10 @@
 //
 // Created by xgg on 05/04/2022.
 //
+
+//
+// Created by xgg on 05/04/2022.
+//
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
@@ -25,12 +29,12 @@ int cmpfunc(const void *a, const void *b) {
 
 int main(int argc, char *argv[]) {
 
-    double start;
+    double start, end_time;
     int opt;
     char *end;
 
     int threads = 1;
-    int arr_size = 100000;
+    int arr_size = 20;
     int b_variant = 0;
     int i_variant = -1;
     // -n size of array
@@ -74,54 +78,67 @@ int main(int argc, char *argv[]) {
                 return 1;
         }
     }
-    omp_set_num_threads(THREADS);
 
+    int *A, *B, *temp;
     int nthreads = -1;
     unsigned int tid;
-    int32_t range = 1000;
+    int32_t range = arr_size;
     int32_t *i_tab = malloc(sizeof(int32_t) * arr_size);
 
-    int n_buckets = (int) arr_size / 100;
+    int n_buckets = (int) arr_size / 5;
     int w = range / n_buckets;
+    int j;
+    int b_index;
 
     struct bucket *buckets;
     buckets = (struct bucket *) calloc(n_buckets, sizeof(struct bucket));
 
-#pragma omp parallel default(none) private(tid)\
-shared(nthreads, i_tab, arr_size, range, n_buckets, buckets, w)
-    {
-        int local_index; // [0 : n_buckets)
-        int real_bucket_index; // [0 : n_buckets * num_threads)
 
-        tid = omp_get_thread_num();
-        if (tid == 0)
-            nthreads = omp_get_num_threads();
-#pragma omp for
-        for (int i = 0; i < arr_size; ++i) {
-            i_tab[i] = (rand_r(&tid) % range);
-        }
+    w = (int)range/n_buckets;
+    A = (int *) malloc(sizeof(int)*arr_size);
+    B = (int *) malloc(sizeof(int)*arr_size);
 
-        // divide into buckets:
-        // v1 read whole array, with offset
-        // == v2 read chunks
-#pragma omp for
-        for (int i = 0; i < arr_size; i++) {
-            local_index = i_tab[i] / w;
-            if (local_index > n_buckets - 1)
-                local_index = n_buckets - 1;
-            real_bucket_index = local_index + tid * n_buckets;
-            buckets[real_bucket_index].n_elem++;
-        }
+    int local_index; // [0 : n_buckets)
+    int real_bucket_index; // [0 : n_buckets * num_threads)
 
+    tid = 0;
+    start = omp_get_wtime();
 
-#pragma omp barrier
-
-        //
-
-        ////
+    for (int i = 0; i < arr_size; ++i) {
+//        i_tab[i] = (rand_r(&tid) % range);
+        i_tab[i] = i;
     }
-    delta += omp_get_wtime() - start;
+    i_tab[0] = 5;
+    i_tab[5] = 10;
 
+    // divide into buckets:
+    // v1 read whole array, with offset
+    // == v2 read chunks
 
-    return 0;
+    for (int i = 0; i < arr_size; i++) {
+        j = i_tab[i] / w;
+        if (j > n_buckets - 1)
+            j = n_buckets - 1;
+        buckets[j].n_elem++;
+    }
+
+    //buckets[0].index=0; //bucket 0 starts from 0 in B, bucket 1 starts from the start of bucket 0 + the number of elements in bucket 0 ...
+    //buckets[0].start=0;
+    for (int i = 1; i < n_buckets; i++) {
+        buckets[i].index = buckets[i - 1].index + buckets[i - 1].n_elem;
+        buckets[i].start = buckets[i - 1].start + buckets[i - 1].n_elem;
+    }
+
+    for (int i = 0; i < arr_size; i++) {
+        j = i_tab[i] / w;
+        if (j > n_buckets - 1)
+            j = n_buckets - 1;
+        b_index = buckets[j].index++;
+        B[b_index] = A[i];
+    }
+
+    end_time = omp_get_wtime();
+
+    printf("delta=%f\n", end_time - start);
+
 }
